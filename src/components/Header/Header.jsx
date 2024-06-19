30b83d61b056ce14ce5dc35909521b2db41696be
SecretCandy <parksw9762@gmail.com>
2024-06-19 14:36:09 +0900

    feat:중복확인 버튼 추가, 이미지 상대경로로 변경/#7

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import logo from './../../assets/3feetLogo.png'
+import React from "react";
+import threeFeetLogo from "./../../assets/threeFeetLogo.png";
+import myPageIcon from "./../../assets/myPageIcon.png";
 
-const Header = () => {
+function Header() {
   return (
     <header className="bg-blue-200 flex items-center justify-center h-[64px] w-[1920px]">
       <center className="flex items-center justify-between h-[64px] w-[1320px]">
         <div className="flex items-center">
-          <img src={logo} alt="logo" className="h-8 w-8 mr-2" />
+          <img src={threeFeetLogo} alt="logo" className="h-8 w-8 mr-2" />
           <span className="font-bold text-2xl">쓰리피트</span>
         </div>
         <div className="flex items-center">
-          <button className="bg-blue-700 text-white px-4 py-1 rounded-[10px] mr-4 hover:bg-blue-900 transition duration-200">로그인</button>
-          <img src="https://cdn-icons-png.flaticon.com/512/1358/1358034.png" alt="user" className="h-6 w-6" />
+          <button className="bg-blue-700 text-white px-4 py-1 rounded-[10px] mr-4 hover:bg-blue-900 transition duration-200">
+            로그인
+          </button>
+          <img src={myPageIcon} alt="user" className="h-6 w-6" />
         </div>
       </center>
     </header>
   );
-};
+}
 
 export default Header;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
+import React from "react";
 
-const LoginPage = () => {
-    return (
-        <div className="flex flex-col items-center justify-center bg-white">
-            <div className="mt-40 bg-white p-8 border-[1.5px] border-blue-300 rounded-[10px] w-[600px] h-[600px]">
-                <h2 className="mt-10 text-3xl font-bold mb-8 text-center">로그인</h2>
-                <form className="px-12">
-                    <div className="mb-10">
-                        <label className="mb-2 text-lg block font-bold">아이디</label>
-                        <input
-                            type="id"
-                            className="w-full h-12 px-3 py-2 border rounded-[10px]"
-                            placeholder="아이디를 입력해 주세요"
-                        />
-                    </div>
-                    <div className="mb-16">
-                        <label className="mb-2 text-lg block font-bold">비밀번호</label>
-                        <input
-                            type="password"
-                            className="w-full h-12 px-3 py-2 border rounded-[10px]"
-                            placeholder="비밀번호를 입력해 주세요"
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="mb-6 h-12 text-lg w-full bg-blue-600 text-white py-2 rounded-[10px] hover:bg-blue-700 transition duration-200"
-                    >
-                        로그인 하기
-                    </button>
-                    <div className="mb-5 underline flex justify-center">아직 회원이 아니신가요?</div>
-                </form>
-            </div>
-        </div>
-    );
-};
+function LoginPage() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-white">
+      <div className="mt-40 bg-white p-8 border-[1.5px] border-blue-300 rounded-[10px] w-[600px] h-[600px]">
+        <h2 className="mt-10 text-3xl font-bold mb-8 text-center">로그인</h2>
+        <form className="px-12">
+          <div className="mb-10">
+            <label className="mb-2 text-lg block font-bold">아이디</label>
+            <input
+              type="id"
+              className="w-full h-12 px-3 py-2 border rounded-[10px]"
+              placeholder="아이디를 입력해 주세요"
+            />
+          </div>
+          <div className="mb-16">
+            <label className="mb-2 text-lg block font-bold">비밀번호</label>
+            <input
+              type="password"
+              className="w-full h-12 px-3 py-2 border rounded-[10px]"
+              placeholder="비밀번호를 입력해 주세요"
+            />
+          </div>
+          <button
+            type="submit"
+            className="mb-6 h-12 text-lg w-full bg-blue-600 text-white py-2 rounded-[10px] hover:bg-blue-700 transition duration-200"
+          >
+            로그인 하기
+          </button>
+          <div className="mb-5 underline flex justify-center">아직 회원이 아니신가요?</div>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default LoginPage;
