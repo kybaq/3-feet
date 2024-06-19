9436a76523afdf721bd1d9433ccc1dc62969f945
kybaq <flfsk12@gmail.com>
2024-06-20 00:08:11 +0900

    style: width 추가

--- src/components/Map/ClubListItem.jsx
+++ src/components/Map/ClubListItem.jsx
@@ -11,11 +11,11 @@ function ClubListItem({ clubs }) {
           return (
             <div
               key={club.id}
-              className="flex justify-center items-center list-none px-4 hover:bg-black-300 hover:bg-opacity-40
-    focus:ring focus:ring-blue-600"
+              className="flex w-full justify-center items-center list-none px-4 hover:bg-black-300 hover:bg-opacity-40
+    focus:ring focus:ring-blue-600 "
             >
               <button onClick={() => setSelectedClub(club.id)}>
-                <img className="h-8 w-10 my-1" src={`${club.logo_url}`} alt="" />
+                <img className="object-fill min-h-8 min-w-10 my-1" src={`${club.logo_url}`} alt="" />
               </button>
             </div>
           );
