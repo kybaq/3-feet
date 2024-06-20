4eae44a943e1e2d25c9fd9e054535dd087b70144
kybaq <flfsk12@gmail.com>
2024-06-20 16:37:44 +0900

    fix: Suspense 사용해 비동기 작업에 의한 UI 렌더링 안되는 문제 해결

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import MapHeader from "../../components/Map/MapHeader";
 import MapSideBar from "../../components/Map/MapSideBar";
 import MapArea from "../../components/Map/MapArea";
@@ -9,15 +9,22 @@ function Map() {
 
   return (
     <NavermapsProvider ncpClientId={CLIENT_ID}>
-      <section className="flex w-screen h-screen justify-center items-center p-10 font-Pretendard">
-        <section className="w-full h-full">
-          <MapHeader />
-          <section id="Map" className="flex justify-center items-center h-[calc(100%-7rem)] gap-8">
-            <MapArea />
-            <MapSideBar />
+      {/* 
+      다양한 컴포넌트가 비동기 로직을 사용하고 있어서 로딩이 안될 시 오류가 발생합니다.
+        따라서 컴포넌트가 정상적으로 렌더링이 되기 위해 데이터가 필요한 상황에,
+        데이터가 없다면 다른 컴포넌트를 먼저 렌더링 하도록 지도와 관련한 모든 요소들을 Suspense 로 감싸주었습니다!
+      */}
+      <Suspense>
+        <section className="flex w-screen h-screen justify-center items-center p-10 font-Pretendard">
+          <section className="w-full h-full">
+            <MapHeader />
+            <section id="Map" className="flex justify-center items-center h-[calc(100%-7rem)] gap-8">
+              <MapArea />
+              <MapSideBar />
+            </section>
           </section>
         </section>
-      </section>
+      </Suspense>
     </NavermapsProvider>
   );
 }
